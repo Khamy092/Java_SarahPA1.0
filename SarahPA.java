3bea9583a72d595ea573bb6f919786df0bad8707
Taqi Khaliqdad <khamy092@mymail.unisa.edu.au>
2024-06-24 14:42:21 +0930

    initial commit.

---
+++ SarahPA.java
@@ -0,0 +1,12 @@
+public class SarahPA {
+    
+    // Main method
+
+    public void main(String[] args) {
+        System.out.println("Welcome to Sarah's Personal Assistant!");
+        System.out.println("How can I help you today?");
+
+        // add code here
+    }
+
+}
