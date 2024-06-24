6204d159d0d210724a3b2aa823e042daeeb06169
Taqi Khaliqdad <khamy092@mymail.unisa.edu.au>
2024-06-24 14:34:33 +0930

    initital commit.

---
+++ input/TextInputHandler.java
@@ -0,0 +1,9 @@
+package input;
+
+public class TextInputHandler {
+    
+    // Main method
+    public void main(String[] args) {
+        System.out.println("Welcome to Sarah's Text Input Handler!");
+    }
+}
