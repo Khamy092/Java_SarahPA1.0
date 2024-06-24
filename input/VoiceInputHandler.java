fc84c2e4fcc65a1b6491e6927f9c8e753e31dc39
Taqi Khaliqdad <khamy092@mymail.unisa.edu.au>
2024-06-24 14:32:06 +0930

    Initital commit.

---
+++ input/VoiceInputHandler.java
@@ -0,0 +1,9 @@
+package input;
+
+public class VoiceInputHandler {
+    
+    // Main method
+    public void main(String[] args) {
+        System.out.println("Welcome to Sarah's Voice Input Handler!");
+    }
+}
