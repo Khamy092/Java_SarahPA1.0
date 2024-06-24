252f7ceb95f566caff7d49162d8d4139993c1f17
Taqi Khaliqdad <khamy092@mymail.unisa.edu.au>
2024-06-24 16:43:34 +0930

    create instance of TextInputHandler and its methods.

--- SarahPA.java
+++ SarahPA.java
@@ -1,12 +1,20 @@
+import input.TextInputHandler;
+
 public class SarahPA {
-    
+
     // Main method
 
     public void main(String[] args) {
         System.out.println("Welcome to Sarah's Personal Assistant!");
         System.out.println("How can I help you today?");
 
-        // add code here
-    }
+        // Create a new TextInputHandler object
+        TextInputHandler textHandler = new TextInputHandler();
 
+        // Take user input
+        String input = textHandler.takeInput();
+        // Process user input
+        textHandler.processInput(input);
+
+    }
 }
